9c25ce3d527027033d3c7d3528dab8c2805fc531
ghostFaceKillah <garmulewicz@gmail.com>
2013-11-06 23:29:24 +0100

    nearly finsihed

--- main.pas
+++ main.pas
@@ -120,23 +120,111 @@ function make_move(this_move_is:input_status; now_playing:player;
     make_move := resu;
   end;
 
+procedure horizontal_indices (var i, j:char; var finished, new_line:boolean);
+  begin
+    finished := false;
+    if (i = 'z') and (j = 'z') then begin
+      i := 'a'; 
+      j := 'A';
+      new_line := true;
+    end else begin
+      inc(j);
+      if j > 'S' then begin
+        inc(i);
+        if i > 's' then 
+          finished := true
+        else begin
+          new_line := true;
+          j := 'A'
+        end;
+      end
+      else
+        new_line := false;
+    end;
+  end;
+
+procedure vertical_indices (var i, j:char; var finished, new_line:boolean);
+  begin
+    finished := false;
+    if (i = 'z') and (j = 'z') then begin
+      i := 'a'; 
+      j := 'A';
+      new_line := true;
+    end else begin
+      inc(i);
+      if i > 's' then begin
+        inc(j);
+        if j > 'S' then 
+          finished := true
+        else begin
+          new_line := true;
+          i := 'a'
+        end;
+      end
+      else
+        new_line := false;
+    end;
+  end;
+
 function new_game_state(plansza:table):state;
+ // podejscie naiwne - wykonujemy tylko 4* 19^2 =~ 1600 operacji, więc dla
+ // komputera nie jest to dużo
   var
     i,j : char;
-  begin 
-    new_game_state := playing;
+    resu_state : state;
+    finished : boolean;
+    just_looked_at : char;
+    new_line : boolean;
+    counter : integer;
+    k : integer;
+  begin
+    resu_state := playing;
+    finished := false;
+    k := 1;
+    while (k <= 2) and (resu_state = playing) do begin
+      i := 'z';
+      j := 'z';  // this means we want to get beginning indices
+      new_line := true;
+      case k of
+        1 : horizontal_indices(i,j,finished, new_line);
+        2 : vertical_indices(i,j,finished, new_line);
+      end;
+      while (resu_state = playing) and not(finished) do begin
+        if new_line then begin
+          just_looked_at := '.'; 
+          counter := 1;
+        end;
+        if plansza[i,j] = just_looked_at then begin
+          inc(counter);
+        end
+        else 
+          counter := 1;
+        just_looked_at := plansza[i,j];
+        if counter >= 6 then 
+          if just_looked_at = 'X' then
+            resu_state := x_wins
+          else if just_looked_at = 'O' then
+            resu_state := o_wins;
+        case k of
+          1 : horizontal_indices(i,j,finished, new_line);
+          2 : vertical_indices(i,j,finished, new_line);
+        end;
+      end;
+      inc(k);
+    end;
+    new_game_state := resu_state;
   end;
 
 begin 
-  
+   
   init_table(plansza);
   game_state := just_started;
   now_playing := X;
   showTable(plansza, now_playing, game_state);
 
-  //// state = ( playing, x_wins, y_wins , just_started,  just_end, draw); 
+  // state = ( playing, x_wins, y_wins , just_started,  just_end, draw); 
 
-  while (game_state = playing) or (game_state = just_started ) do begin
+  while (game_state = playing) or (game_state = just_started) do begin
     readln(napis);
     input_is := input_analyser(napis);
     if input_is = empty then
@@ -150,5 +238,5 @@ begin
       end
     end;
       showTable(plansza, now_playing, game_state);
-  end;
+  end; 
 end.
